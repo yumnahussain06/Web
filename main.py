365b023f4a8a3c031ac044b4b2823f9272946d33
yumnahussain06 <yumnahussain06@gmail.com>
2026-05-31 23:48:17 +0500

    upgraded main file

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ import json
 from mongodb import metrics_collection
 from mongodb import predictions_collection
 
+from security import (hash_password, verify_password)
+from auth import create_access_token
+from mongodb import users_collection
+
+from fastapi import Depends, HTTPException, Header
+from auth import verify_token
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    username = verify_token(token)
+
+    if not username:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return username
+
+
 
 # LOAD MODEL + PIPELINE
 model = joblib.load("Ml/best_loan_risk_model.pkl")
@@ -20,6 +41,17 @@ preprocessing_pipeline = joblib.load(
 
 # FASTAPI APP
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # INPUT SCHEMA
@@ -40,6 +72,12 @@ class LoanInput(BaseModel):
     Property_Area: Literal['Urban', 'Semiurban', 'Rural'] = Field(...)
 
 
+class UserRegister(BaseModel):
+
+    username: str =  Field(...)
+
+    password: str =  Field(...)
+
 # HOME ROUTE
 @app.get("/")
 def home():
@@ -51,28 +89,22 @@ def home():
 
 # PREDICTION ROUTE
 @app.post("/predict")
-def predict(data: LoanInput):
-
+def predict(data: LoanInput, current_user: str = Depends(get_current_user)):
     input_df = pd.DataFrame([data.model_dump()])
-
     prediction = model.predict(input_df)
+    
+    result = prediction[0]
+    status = "Approved" if result in [1, 'Y'] else "Rejected"
 
     predictions_collection.insert_one({
-
-    "input_data": input_df.to_dict(),
-
-    "prediction": str(result)
-})
-
-    result = prediction[0]
-
-    status = "Approved" if result in [1, 'Y'] else "Rejected"
+        "input_data": data.model_dump(),
+        "prediction": str(result)
+    })
 
     return {
         "loan_status_code": str(result),
         "prediction": status
     }
-
 
 @app.on_event("startup")
 def save_metrics():
@@ -86,11 +118,60 @@ def save_metrics():
 
 
 @app.get("/metrics")
-def get_metrics():
+def get_metrics(current_user: str = Depends(get_current_user)):
 
     metrics = metrics_collection.find_one(
         {},
         {"_id": 0}
     )
 
-    return metrics        
+    return metrics     
+
+
+@app.post("/register")
+def register(user: UserRegister):
+
+    existing_user = users_collection.find_one(
+        {"username": user.username}
+    )
+
+    if existing_user:
+
+        return {
+            "message":
+            "Username already exists"
+        }
+
+    hashed_password = hash_password(
+        user.password
+    )
+
+    users_collection.insert_one({
+
+        "username": user.username,
+
+        "hashed_password": hashed_password
+    })
+
+    return {
+        "message":
+        "User registered successfully"
+    }
+
+
+@app.post("/token")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    db_user = users_collection.find_one({"username": form_data.username})
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid username")
+
+    if not verify_password(form_data.password, db_user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    token = create_access_token({"sub": form_data.username})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
